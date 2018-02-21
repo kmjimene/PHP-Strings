bddf1046cd8fdf15a75aeb4423a3c9dc28cdc96a
Katherine <kmjimene@users.noreply.github.com>
2018-02-20 17:31:51 -0800

    Create concatenatingStrings.php

---
+++ concatenatingStrings.php
@@ -0,0 +1,10 @@
+<?php
+$name = 'Kat';
+$string_one = "Learning to display \"Hello " . $name . "!\" to the screen.\n";
+echo $string_one;
+
+$firstName = 'Rasmus';
+$lastName = 'Lerdorf';
+$fullName = $firstName . " " . $lastName;
+echo $fullName;
+?>
